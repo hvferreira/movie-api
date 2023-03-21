8ae37ae20ed9a23fb93bcebdf5273cfcb89f8cc6
hvferreira <heldervferreira@gmail.com>
2023-03-21 18:27:30 +0000

    testing /health Done and testing moviebyId start

--- src/test/java/com/movie/api/controller/MovieControllerTest.java
+++ src/test/java/com/movie/api/controller/MovieControllerTest.java
@@ -3,9 +3,8 @@ package com.movie.api.controller;
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.movie.api.model.Movie;
 import com.movie.api.service.MovieService;
 import org.junit.jupiter.api.Test;
 
@@ -15,12 +14,15 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 //@ExtendWith(StringEx.class)
 @WebMvcTest(MovieController.class)
@@ -34,13 +36,8 @@ class MovieControllerTest {
     @Test
     void movieByID() throws Exception {
         RequestBuilder request = MockMvcRequestBuilders.get("/api/v1/movie/20");
-        System.out.println("HHEEELLLOOOO " + request);
         MvcResult result = mockMvc.perform(request).andReturn();
-        //assertEquals("OLA", result.getResponse().getContentAsString());
-
-        //*********************************************************************************
-        //*********************************************************************************
-
+        assertEquals("oi", result.getResponse().getContentType());
 
     }
 
@@ -76,7 +73,19 @@ class MovieControllerTest {
 
     @Test
     void health() throws Exception {
-        this.mockMvc.perform(get("/health")).andDo(print()).andExpect(status().isOk())
-                .andExpect(content().string(containsString(Health.up().toString())));
+        // this.mockMvc.perform(get("/api/v1/movie/health")).andDo(print()).andExpect(status().isOk())
+        //       .andExpect(content().string(containsString(String.valueOf(Health.up().build()))));
+
+        RequestBuilder request = MockMvcRequestBuilders.get("/api/v1/movie/health");
+        MvcResult result = mockMvc.perform(request).andReturn();
+        assertEquals("{\"status\":\"UP\"}", result.getResponse().getContentAsString());
+
+        //mockMvc.perform(get("/actuator/health/random"))
+        //      .andExpect(status().isNotFound());
+
+        //mockMvc.perform(get("/actuator/health/random"))
+        //      .andExpect(jsonPath("$.status").exists())
+        //    .andExpect(jsonPath("$.details.strategy").value("thread-local"))
+        //  .andExpect(jsonPath("$.details.chance").exists());
     }
 }
