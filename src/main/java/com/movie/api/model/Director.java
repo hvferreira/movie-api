8b9bd106bc80d56d9d57dfd6f87c2458d2de1d81
dwright1989 <debbiewright@live.co.uk>
2023-03-24 15:05:20 +0000

    Director test changed - removal of constructor

--- src/main/java/com/movie/api/model/Director.java
+++ src/main/java/com/movie/api/model/Director.java
@@ -14,8 +14,4 @@ public class Director extends Person{
     private String job;
 
 
-    public Director(Long id, String name, String known_for_department, String birthday, String biography, String job) {
-        super(id, name, known_for_department, birthday, biography);
-        this.job = job;
-    }
 }
